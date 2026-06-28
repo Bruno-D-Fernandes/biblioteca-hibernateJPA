5f99d60406bcaad5ff45573c7a390def1f4b076c
Bruno  de Paula Fernandes <BrunoFernandes2315@outlook.com>
2026-06-27 22:43:37 -0300

    Hibernate entity lifecycle finalizado

--- src/main/java/edu/hibernate/service/LivroService.java
+++ src/main/java/edu/hibernate/service/LivroService.java
@@ -3,6 +3,7 @@ package edu.hibernate.service;
 import edu.hibernate.dto.LivroDTO.LivroResponseDTO;
 import edu.hibernate.entity.Livro;
 import edu.hibernate.repository.LivroRepository;
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,22 +14,30 @@ import java.util.List;
 public class LivroService {
 
     private LivroRepository livroRepository;
+    private EntityManager entityManager;
 
     @Autowired
-    public LivroService(LivroRepository livroRepository) {
+    public LivroService(LivroRepository livroRepository, EntityManager entityManager) {
         this.livroRepository = livroRepository;
+        this.entityManager = entityManager;
     }
-
 
     @Transactional
     public List<LivroResponseDTO> getAllLivroCategoriaAutor() {
 
         List<Livro> allLivroCategoriaAutor = livroRepository.getAllLivroCategoriaAutor();
 
-        //Técnicamente eles estão MANAGED, então ao final, todos os livro se tornaram "Trocado" no banco
+        //Um detached deixa de ser rastreado pelo hibernate, bem "parecido" com o trancient
+        // vai retornar com o título trocado para o usuário, porém no banco nada muda
         for (Livro livro : allLivroCategoriaAutor) {
+            entityManager.detach(livro);
             livro.setTitulo("Trocado");
         }
+
+        for (Livro livro : allLivroCategoriaAutor) {
+            livroRepository.delete(livro);
+        }
+
 
         List<LivroResponseDTO> livroResponseDTOS = allLivroCategoriaAutor.stream().
                 map(LivroResponseDTO::new)
